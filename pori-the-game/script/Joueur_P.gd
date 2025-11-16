6173b63244435cc90764c8678d5d8b46aca3292f
Poridaby <vergottini.adrien@gmail.com>
2025-11-16 17:28:02 +0100

    Update Joueur_P.gd

--- pori-the-game/script/Joueur_P.gd
+++ pori-the-game/script/Joueur_P.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-@export var speed = 400
+@export var speed = 400                #Declaration des variables
 var screen_size
 
-func _ready():
+func _ready():                                       #Fonction qui permet d'ouvrir la fenêtre
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
+func _process(delta):                    #Fonction qui permet d'assigner les actions des touches  
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -17,7 +17,7 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 		
-	if velocity.length() > 0:
+	if velocity.length() > 0:                         #Endroit qui permet de déclencher ou non l'animation
 		velocity = velocity.normalized() * speed;
 		$AnimatedSprite2D.play()
 	else:
@@ -26,7 +26,7 @@ func _process(delta):
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.x != 0:
+	if velocity.x != 0:                                    #permet de changer d'animation en fonction de si le perso se déplace en vertical ou en horizontal
 		$AnimatedSprite2D.animation = "marche"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
