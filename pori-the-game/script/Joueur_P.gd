496daa067d389607a17f869228322c5f6aeeb891
enzo martin <maop76698@gmail.com>
2025-11-23 16:59:46 +0100

    patch anim interact

--- pori-the-game/script/Joueur_P.gd
+++ pori-the-game/script/Joueur_P.gd
@@ -21,26 +21,27 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("move_down"):
 		vel.y += 1
 
+
+	if global_var.player_can_move == true:
 	#Endroit qui permet de déclencher ou non l'animation
-	if vel.length() > 0:
-		vel = vel.normalized() * speed
-		$AnimatedSprite2D.play()
+		if vel.length() > 0:
+			vel = vel.normalized() * speed
+			$AnimatedSprite2D.play()
+		else:
+			$AnimatedSprite2D.stop()
+		velocity = vel
+		# move_and_slide() gère automatiquement les collisions
+		# et empêche le personnage de traverser un StaticBody2D
+		move_and_slide()
+
+		# Si déplacement horizontal
+		if vel.x != 0:
+			$AnimatedSprite2D.animation = "marche"
+			# Retourne le sprite si on va à gauche
+			$AnimatedSprite2D.flip_h = vel.x < 0  
+		# Sinon si déplacement vertical  
+		elif vel.y != 0:                    
+			$AnimatedSprite2D.animation = "haut"
+
 	else:
 		$AnimatedSprite2D.stop()
-	if global_var.player_can_move == true:
-		velocity = vel
-	else:
-		velocity = Vector2(0,0)
-
-	# move_and_slide() gère automatiquement les collisions
-	# et empêche le personnage de traverser un StaticBody2D
-	move_and_slide()
-
-	# Si déplacement horizontal
-	if vel.x != 0:
-		$AnimatedSprite2D.animation = "marche"
-		# Retourne le sprite si on va à gauche
-		$AnimatedSprite2D.flip_h = vel.x < 0  
-	# Sinon si déplacement vertical  
-	elif vel.y != 0:                    
-		$AnimatedSprite2D.animation = "haut"
