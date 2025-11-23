71ed04123099454013154c68d95c7c377034ac5d
enzo martin <maop76698@gmail.com>
2025-11-23 17:00:30 +0100

    docstring

--- pori-the-game/script/global_var.gd
+++ pori-the-game/script/global_var.gd
@@ -1,4 +1,7 @@
 extends Node
 
+# Décide si le joueur a le droit de bouger
 var player_can_move = true
+
+# Texte a afficher dans la boite de dialogue
 var texte_dialogue = "Ereur"
--- pori-the-game/script/texte_dialogue.gd
+++ pori-the-game/script/texte_dialogue.gd
@@ -1,4 +1,9 @@
 extends RichTextLabel
 
 func _process(_delta):
+	"""
+	Actualise en temps réel le texte de la boite de dialogue
+	:param _delta: a laisser, jamais utilisé mais obligatoire pour que la fonction soit reconnue
+	:comportement: changer le texte de la boite de dialogue
+	"""
 	set_text(global_var.texte_dialogue)
