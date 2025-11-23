4d8ff7a6d440406eaaecc68e6331945c477e96cd
Poridaby <vergottini.adrien@gmail.com>
2025-11-23 16:37:57 +0100

    warp

--- pori-the-game/script/warp.gd
+++ pori-the-game/script/warp.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var next_scene_right:String
+@export var next_scene_left:String
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,18 +10,25 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 
-func _on_exit_body_entered(body: Node2D) -> void:
-	var next_scene = load("res://scenes/plaine.tscn")
+func _on_exit_body_entered(_body: Node2D) -> void:
+	var next_scene = load(next_scene_right)
 	var next_scene_instances = next_scene.instantiate()
 	next_scene_instances.init_player_position(Vector2.RIGHT)
 	get_tree().root.add_child(next_scene_instances)
 	queue_free()
-	
+
+func _on_exit_left_body_entered(_body: Node2D) -> void:
+	var next_scene = load(next_scene_left)
+	var next_scene_instances = next_scene.instantiate()
+	next_scene_instances.init_player_position(Vector2.LEFT)
+	get_tree().root.add_child(next_scene_instances)
+	queue_free()
+
 func init_player_position(direction: Vector2):
 	if direction == Vector2.RIGHT:
 		$Joueur_P.position = $SpawnerLeft.position
--- pori-the-game/script/warp_zone.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _process(_delta):
-	if overlaps_body($/root/Main/Joueur_P):
-		
