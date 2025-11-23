2b14ac0b9fecbc90fbc7823eca0fb73a1224bf40
enzo martin <maop76698@gmail.com>
2025-11-23 17:00:53 +0100

    opti + docstring

--- pori-the-game/script/warp.gd
+++ pori-the-game/script/warp.gd
@@ -4,18 +4,11 @@ extends Node2D
 @export var next_scene_left:String
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
-
 func _on_exit_body_entered(_body: Node2D) -> void:
+	"""
+	Je sais pas ce que ca fait
+	:comportement: pori fait ca for pitty
+	"""
 	var next_scene = load(next_scene_right)
 	var next_scene_instances = next_scene.instantiate()
 	next_scene_instances.init_player_position(Vector2.RIGHT)
@@ -23,6 +16,10 @@ func _on_exit_body_entered(_body: Node2D) -> void:
 	queue_free()
 
 func _on_exit_left_body_entered(_body: Node2D) -> void:
+	"""
+	Je sais pas ce que ca fait
+	:comportement: pori fait ca for pitty
+	"""
 	var next_scene = load(next_scene_left)
 	var next_scene_instances = next_scene.instantiate()
 	next_scene_instances.init_player_position(Vector2.LEFT)
@@ -30,6 +27,10 @@ func _on_exit_left_body_entered(_body: Node2D) -> void:
 	queue_free()
 
 func init_player_position(direction: Vector2):
+	"""
+	Choisi ou faire apparaitre de joueur
+	:comportement: choisi le bon spawner pour le joueur
+	"""
 	if direction == Vector2.RIGHT:
 		$Joueur_P.position = $SpawnerLeft.position
 	elif direction == Vector2.LEFT:
