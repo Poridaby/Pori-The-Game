680ed3178759e205b97183a4fac3c71e8022a84b
Poridaby <vergottini.adrien@gmail.com>
2025-12-21 16:59:38 +0100

    warp cassée

--- pori-the-game/scenes/baseenemy.gd
+++ pori-the-game/scenes/baseenemy.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
-
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/combat.tscn")
+	if body is Player:
+		get_tree().change_scene_to_file("res://scenes/combat.tscn")
--- pori-the-game/script/warp.gd
+++ pori-the-game/script/warp.gd
@@ -4,35 +4,49 @@ extends Node2D
 @export var next_scene_left:String
 
 
-func _on_exit_body_entered(_body: Node2D) -> void:
-	"""
-	Teleporte le joueur d'une zone a l'autre
-	:comportement: change de scene
-	"""
-	var next_scene = load(next_scene_right)
-	var next_scene_instances = next_scene.instantiate()
-	next_scene_instances.init_player_position(Vector2.RIGHT)
-	get_tree().root.add_child(next_scene_instances)
-	queue_free()
+#func _on_exit_body_entered(_body: Node2D) -> void:
+	#"""
+	#Teleporte le joueur d'une zone a l'autre
+	#:comportement: change de scene
+	#"""
+	#var next_scene = load(next_scene_right)
+	#var next_scene_instances = next_scene.instantiate()
+	#next_scene_instances.init_player_position(Vector2.RIGHT)
+	#get_tree().root.add_child(next_scene_instances)
+	#queue_free()
 
-func _on_exit_left_body_entered(_body: Node2D) -> void:
-	"""
-	Teleporte le joueur d'une zone a l'autre
-	:comportement: change de scene
-	"""
-	var next_scene = load(next_scene_left)
-	var next_scene_instances = next_scene.instantiate()
-	next_scene_instances.init_player_position(Vector2.LEFT)
-	get_tree().root.add_child(next_scene_instances)
-	queue_free()
+#func _on_exit_left_body_entered(_body: Node2D) -> void:
+	#"""
+	#Teleporte le joueur d'une zone a l'autre
+	#:comportement: change de scene
+	#"""
+	#var next_scene = load(next_scene_left)
+	#var next_scene_instances = next_scene.instantiate()
+	#next_scene_instances.init_player_position(Vector2.LEFT)
+	#get_tree().root.add_child(next_scene_instances)
+	#queue_free()
+	
 
-func init_player_position(direction: Vector2):
-	"""
-	Choisi ou faire apparaitre de joueur
-	:comportement: choisi le bon spawner pour le joueur
-	"""
-	if direction == Vector2.RIGHT:
-		$Joueur_P.position = $SpawnerLeft.position
-	elif direction == Vector2.LEFT:
-		$Joueur_P.position = $SpawnerRight.position
+
+#func init_player_position(direction: Vector2):
+	#"""
+	#Choisi ou faire apparaitre de joueur
+	#:comportement: choisi le bon spawner pour le joueur
+	#"""
+	#if direction == Vector2.RIGHT:
+		#$Joueur_P.position = $SpawnerLeft.position
+	#elif direction == Vector2.LEFT:
+		#$Joueur_P.position = $SpawnerRight.position
+		
+
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().change_scene_to_file("res://scenes/plaine.tscn")
+		global_position = Vector2.ZERO
+
+
+func _on_exit_left_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().change_scene_to_file("res://scenes/Inser_ellenon.tscn")
 		
