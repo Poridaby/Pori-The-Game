0d2a5e89abd5d45ea95b78ed5a06a4c931bd4661
Poridaby <vergottini.adrien@gmail.com>
2025-12-27 12:45:57 +0100

    fhjzefvbguh

--- pori-the-game/script/combat_script/controle.gd
+++ pori-the-game/script/combat_script/controle.gd
@@ -1,6 +1,1 @@
 extends Control
-
-var perso_stats: Tonar
-
-func set_perso_stats(stats: Tonar) -> void:
-	perso_stats = stats
--- pori-the-game/script/enemy_script/ptitcrote_stats.gd
+++ pori-the-game/script/enemy_script/ptitcrote_stats.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name Ptitcrote
 
 @export var stats: Stats
 
