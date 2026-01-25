67543ae4e1b88d6e4a43fccf34bf4e60f38c08fe
Poridaby <vergottini.adrien@gmail.com>
2026-01-25 15:24:14 +0100

    dsgdrhgdry

--- pori-the-game/script/combat_script/PlayerManager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-@export var player_base_stats: Stats
-
-func _ready():
-	player_base_stats = load("res://script/class_script/TonarStats.tres")
--- pori-the-game/script/enemy_script/ptitecrote_attack.gd
+++
@@ -1,6 +0,0 @@
-extends Attack
-class_name PtitecroteAttack
-
-func execute(): 
-	print("L'ennemi charge")
-	finished.emit()
