7a48c47a1ece77f3150b542ae4200b8dc7fd31d6
frite_des_iles <maop76698@gmail.com>
2026-02-16 18:55:40 +0100

    Update lancer_combat.gd

--- pori-the-game/script/combat/lancer_combat.gd
+++ pori-the-game/script/combat/lancer_combat.gd
@@ -3,7 +3,26 @@ extends Node
 @export var stats_class_local: combat_class
 
 func _ready():
-	var infos = recup_infos(0)
+	var regions = [["inserelenon",["ptitcrote","Carotte_maléfique"]]
+		]
+	var combat = recup_infos(0)
+	get_tree().change_scene_to_packed(combat[0])
+	var nbr_ennemi = combat[1]
+	var region_actuelle = combat[2]
+	var ennemi_principal = combat[3]
+	var mob_region = ["ennemie shiny de fou malade ultra rare (impossible a avoir)"]
+
+	for region in regions:
+			if region[0] == region_actuelle:
+				mob_region = region[1]
+	
+	var ennemis = []
+	for i in range(0, nbr_ennemi):
+		if i == 0:
+			ennemis.append(ennemi_principal)
+		else:
+			ennemis.append(mob_region.pick_random())
+	
 
 
 func recup_infos(id_combat):
