dadc3d7d897791c824953b651ad1ce859498481b
Poridaby <vergottini.adrien@gmail.com>
2026-02-16 16:40:04 +0100

    inventaire recup item

--- pori-the-game/script/inventory/Inventory.gd
+++ pori-the-game/script/inventory/Inventory.gd
@@ -3,18 +3,19 @@ extends Node
 # Variable de l'inventaire
 var inventory = []
 
-# Signals customes
-signal inventory_updated
-
 # Scene et Node Référence
 var player_node: Node = null
+
+# Signals customes
+signal inventory_updated
 
 func _ready():
 	# Le stockage de l'inventaire est initialisé à 30 slots
 	inventory.resize(30)
 
 # Rajoute un item dans l'inventaire
-func add_item():
+@warning_ignore("unused_parameter")
+func add_item(item):
 	inventory_updated.emit()
 	
 # Retire un item de l'inventaire
--- pori-the-game/script/inventory/inventoty_item.gd
+++ pori-the-game/script/inventory/inventoty_item.gd
@@ -20,3 +20,18 @@ func _process(_delta):
 	# Set la texture pour l'afficher dans l'éditeur
 	if Engine.is_editor_hint():
 		icon_sprite.texture = item_texture
+
+# Ajoute l'item dans l'inventaire
+func pickup_item():
+	var item = {
+		"quantity": 1,
+		"type": item_type,
+		"name": item_name,
+		"texture": item_texture,
+		"effect": item_effect,
+		"scene_path": scene_path
+	}
+	if Inventory.player_node:
+		Inventory.add_item(item)
+		self.queue_free()
+		
