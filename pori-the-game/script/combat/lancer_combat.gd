3f98d595f9f77c8e7fd8d7036abc1d7eb3158e3d
frite_des_iles <maop76698@gmail.com>
2026-02-16 18:25:17 +0100

    upgrade lancer combat

--- pori-the-game/script/combat/lancer_combat.gd
+++ pori-the-game/script/combat/lancer_combat.gd
@@ -10,7 +10,10 @@ func recup_infos(id_combat):
 	var combats_possibles = [
 	preload("res://script/stats/ressource combat/test.tres"),
 ]
-
-	print(combats_possibles)
+	# Renvoie la bonne ressource
 	var combat = combats_possibles[id_combat]
-	return combat.scene
+	var scene = combat.scene
+	var nbr_ennemi = combat.nbr_ennemi
+	var region = combat.region
+	var ennemi_principal = combat.ennemi_pricipal
+	return [scene, nbr_ennemi, region, ennemi_principal]
