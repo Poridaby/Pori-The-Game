f6fbe1d20380318dda7db2370cfb6f9664e96be1
frite_des_iles <maop76698@gmail.com>
2026-02-17 14:19:04 +0100

    ajout de comentaires + deplacement d'un fichier mal placé

--- pori-the-game/script/combat/combat.gd
+++ pori-the-game/script/combat/combat.gd
@@ -1,24 +1,32 @@
 extends Node2D
 
+# Initialisation des variables globales au script
 var ennemis_a_spawn = []
-var ennemi1_stat = []
-var ennemi2_stat = []
-var ennemi3_stat = []
+var ennemi1_stats = null
+var ennemi2_stats = null
+var ennemi3_stats = null
 
+# Fonction qui récupère les ennemis a générer
 func setup_combat(liste_ennemis):
 	ennemis_a_spawn = liste_ennemis
 
+
 func _ready():
 	spawn_ennemis()
-	print(ennemi1_stat,ennemi2_stat,ennemi3_stat)
+	# Utile au tests
+	print(ennemi1_stats,ennemi2_stats,ennemi3_stats)
 
+# Fait apparaître les ennemis sur leurs spawners et récupère leurs stats
 func spawn_ennemis():
-
+	# Liste contenant les spawners
 	var spawners = $Spawners_ennemi.get_children()
+	var stats = []
 
 	for i in range(ennemis_a_spawn.size()):
 
+		# Récupère l'ennemi actuel
 		var mob_name = ennemis_a_spawn[i]
+		# Le fait apparaite sur son spawner
 		var mob_scene = load("res://scenes/enemy_combat/" + mob_name + ".tscn")
 
 		if mob_scene == null:
@@ -27,14 +35,16 @@ func spawn_ennemis():
 
 		var mob_instance = mob_scene.instantiate()
 		mob_instance.global_position = spawners[i].global_position
-		if i == 0:
-			ennemi1_stat = recup_infos(mob_name)
-		elif i == 1:
-			ennemi2_stat = recup_infos(mob_name)
-		elif i == 2:
-			ennemi3_stat = recup_infos(mob_name)
+		
+		# Récupère les stats de chaques mobs
+		stats.append(recup_infos(mob_name))
 
 		add_child(mob_instance)
+	
+	# Stocke les stats de chaques mobs individuellement
+	ennemi1_stats = stats[0]
+	ennemi2_stats = stats[1]
+	ennemi3_stats = stats[2]
 
 
 func recup_infos(ennemi):
--- pori-the-game/script/combat/lancer_combat.gd
+++ pori-the-game/script/combat/lancer_combat.gd
@@ -1,3 +1,4 @@
+# Script dans l'autoload, la fonction combattre est utilisable partout
 extends Node
 
 @export var stats_class_local: combat_class
@@ -13,21 +14,28 @@ func combattre(id_combat):
 	# La variable combat stocke toutes les infos du combat a lancer
 	var combat = recup_infos(id_combat)
 
+	# Récupère individuellement chaques information sur le combat
 	var scene = combat[0]
 	var nbr_ennemi = combat[1]
 	var region_actuelle = combat[2]
 	var ennemi_principal = combat[3]
 
+	# Récupère la liste des mobs dans la région actuelle
 	var mob_region = regions.get(region_actuelle, [])
 
 	var ennemis = []
-
+	
+	# Choisi les ennemis en fonction du nombre d'ennemis requis
 	for i in range(nbr_ennemi):
+		# Le premier ennemi est celui touch par le joueur sur la map
 		if i == 0:
 			ennemis.append(ennemi_principal)
+		# Les autres sont choisi au hasard parmis ceux de la région
 		else:
 			ennemis.append(mob_region.pick_random())
 
+	# Change de scene en lui passant la liste d'ennemis via la fontion setup_combat
+	# Passe le relais au script res://script/combat/combat.gd
 	var combat_instance = scene.instantiate()
 	combat_instance.setup_combat(ennemis)
 
@@ -37,13 +45,16 @@ func combattre(id_combat):
 
 
 func recup_infos(id_combat):
-
+	
+	# Liste contenant touts les .tres de combats
 	var combats_possibles = [
 		preload("res://script/stats/ressource combat/test.tres")
 	]
 
+	# Choisi le bon combat via l'id fourni
 	var combat = combats_possibles[id_combat]
 
+	# Renvoie les infos du combat
 	return [
 		combat.scene,
 		combat.nbr_ennemi,
